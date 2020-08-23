3526f86ad1d39e58abc7b84869343916696d161a
Blake Chalmers (1000064560) <CHALBR1@student.op.ac.nz>
2020-08-24 09:53:54 +1200

    finished work

--- mvt/polls/views.py
+++ mvt/polls/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Question
 
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Hello')
+    latest_question_list = Question.objects.order_by('-pub_date')
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+def detail(request,question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'polls/detail.html', context)
